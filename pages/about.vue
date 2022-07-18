4467bb550754e131dfb3a5721004875238874862
faviolla <32761891+faviolla@users.noreply.github.com>
2022-07-18 12:02:00 +0300

    uploading files
    uploading files by web

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum quisquam dolorem amet sequi facere sapiente quae eligendi perspiciatis ut corporis odit et non labore, sunt iusto fugiat numquam? Fuga, dolor!</p>
+
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus blanditiis nesciunt natus porro eius provident, aut facilis ex soluta, numquam voluptate fuga? Labore nobis totam nostrum cupiditate sint repellendus harum.</p>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam dicta ratione similique natus ducimus laboriosam fugiat neque iusto corporis quo magni facilis a, incidunt vero molestias distinctio eaque sequi recusandae.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
